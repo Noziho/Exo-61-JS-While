2d36c524c6de330ede6a40ecf57cf24fca9c2d9c
Noah2211 <noah.decroix3@gmail.com>
2021-11-15 16:19:23 +0100

    exo 1/2/3/4

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-var i = 15;
-while (i   ) {
+let i = 15;
+while (i < 60  ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+    i+=15 ;
 }
-
-
